c1b4f24fb979ea6a0893afa35789baadc39866f1
EsleySaab <esleysantana.sa@gmail.com>
2025-01-26 13:50:10 -0300

    chore: add sonner

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { toast } from "sonner";
 
 import { Label } from "../../components/ui/label";
 import { Button } from "../../components/ui/button";
@@ -22,9 +23,20 @@ export function SignIn() {
   } = useForm<SignInForm>();
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data);
+    try {
+      console.log(data);
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      toast.success("Enviamos um link de autenticação para o seu e-mail.", {
+        action: {
+          label: "Reenviar",
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch (error) {
+      toast.error("Credenciais inválidas.");
+    }
   }
 
   return (
