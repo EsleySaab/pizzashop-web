03ebfc0de2a1fbec7243d460faa13a1a3d0b51bc
EsleySaab <esleysantana.sa@gmail.com>
2025-01-30 01:03:38 -0300

    feat: add revenue graph in the period

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,70 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../../../components/ui/card";
+
+import colors from "tailwindcss/colors";
+
+import {
+  ResponsiveContainer,
+  LineChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Line,
+  Tooltip,
+} from "recharts";
+
+const data = [
+  { date: "30/01", revenue: 1200 },
+  { date: "31/01", revenue: 800 },
+  { date: "01/02", revenue: 900 },
+  { date: "02/02", revenue: 400 },
+  { date: "03/02", revenue: 2300 },
+  { date: "04/02", revenue: 800 },
+  { date: "05/02", revenue: 640 },
+];
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receita diário no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
+
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })
+              }
+            />
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet["500"]}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  );
+}
